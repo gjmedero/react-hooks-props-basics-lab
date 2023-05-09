4c9201d65d54672347d212b00a79b7580d05b8fd
Gilberto Medero <gjmedero@gmail.com>
2023-05-09 16:32:05 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  let bioCheck = true;
+
+  if (props.bio === undefined || props.bio.length === 0) {
+    bioCheck = false;
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioCheck &&
+        <p>{props.bio}</p>
+      }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links {...props.links} />
     </div>
   );
 }
